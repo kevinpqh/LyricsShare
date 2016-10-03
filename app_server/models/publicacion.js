2114c7b1b646ef0de9105e093acd8d8d6c9062c7
kevin <kevinxpt27@gmail.com>
2016-10-03 15:22:15 -0500

    mongoose

---
+++ app_server/models/publicacion.js
@@ -0,0 +1,17 @@
+var mongoose = require( 'mongoose' );
+
+var reviewSchema = new mongoose.Schema({
+   
+});
+
+var openingTimeSchema = new mongoose.Schema({
+
+});
+
+var locationSchema = new mongoose.Schema({
+    
+});
+
+//mongoose.model('Publicacion', locationSchema);
+
+
